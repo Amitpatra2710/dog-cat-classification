b95a342b6353a7c64ef24130c54f546b2ac57dc5
Amit Patra <amitbaranpatra@gmail.com>
2023-03-14 17:17:06 +0530

    Update setup

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name="Cat-Dog",
     version="0.0.1",
-    author="iNeuron",
-    author_email="query@ineuron.ai",
+    author="amit",
+    author_email="amitbaranpatra@gmail.com",
     packages=find_packages(),
     install_requires=[],
+
 )
